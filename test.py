af1b781b30156ef676b264b7710db77b9ecb90cb
Kamil <77681807+abrakadabror@users.noreply.github.com>
2023-08-23 12:48:18 +0200

    quick test - counter

--- test.py
+++ test.py
@@ -1,2 +1,7 @@
-vaule_01 = 'ASSIGNMENT: Set Operations'
-print(vaule_01.lower().replace(' ', ''))
+# vaule_01 = 'ASSIGNMENT: Set Operations'
+# print(vaule_01.lower().replace(' ', ''))
+
+
+for box in range(50):
+    print("Package A")
+    print("Task complete")
