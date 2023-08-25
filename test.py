08ec60b6ae52201aa5a8d21985e11bc95b64319b
Kamil <77681807+abrakadabror@users.noreply.github.com>
2023-08-25 10:01:52 +0200

    Creating a simple counting machine. Ended with word "Kaboom"

--- test.py
+++ test.py
@@ -1,8 +1,21 @@
-counter = 0
-# counter = int(input('Podaj liczbe w zakresie od 0 do 10: '))
-# if counter > 10:
-#     print('Podales liczbe z poza zakresu')
-while counter < 5:
-    print('licznik', counter)
-    counter += 1
-print('zadanie zakonczone')
+# from time import sleep
+
+# counter = 0
+# # counter = int(input('Podaj liczbe w zakresie od 0 do 10: '))
+# # if counter > 10:
+# #     print('Podales liczbe z poza zakresu')
+# while counter < 3:
+#     print('licznik', counter)
+#     sleep(1)
+#     counter += 1
+# print('Kabooom')
+
+
+# pojemink_kamil = 4.5
+# print(type(pojemink_kamil))
+import time
+from time import sleep
+for nasz_pojemnik in range(10):
+    sleep(1)
+    print(nasz_pojemnik)
+print('Kabooom')
