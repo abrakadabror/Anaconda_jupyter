85cb46e2926df5980184c1b841f2ffcbdd6a3fd4
Kamil <77681807+abrakadabror@users.noreply.github.com>
2023-08-26 14:15:45 +0200

    added a limit of games won by one player

--- test_01.py
+++ test_01.py
@@ -1,20 +1,18 @@
-user_choice_1 = input(
-    str('Graczu numer 1 wybierz jedna z opcji: kamien/papier/nozyce '))
-user_choice_2 = input(
-    str('Graczu numer 2 wybierz jedna z opcji: kamien/papier/nozyce '))
+player1_score = 0
+player2_score = 0
+while player1_score != 3 and player2_score != 3:
+    user_choice_1 = input(
+        str('Graczu numer 1 wybierz jedna z opcji: kamien/papier/nozyce '))
+    user_choice_2 = input(
+        str('Graczu numer 2 wybierz jedna z opcji: kamien/papier/nozyce '))
 
-if user_choice_1 == 'papier' and user_choice_2 == 'kamien' or \
-        user_choice_1 == 'kamien' and user_choice_2 == 'nozyce' or  \
-        user_choice_1 == 'nozyce' and user_choice_2 == 'papier':
-    print('Gracz1 wygral')
-elif user_choice_1 == user_choice_2:
-    print('remis')
-else:
-    print('Gracz dwa wygral')
-
-
-# pojemnik = 4
-# pojemnik2 = 5
-
-# if pojemnik == 4 and pojemnik2 == 5:
-#     print('dziala')
+    if user_choice_1 == 'papier' and user_choice_2 == 'kamien' or \
+            user_choice_1 == 'kamien' and user_choice_2 == 'nozyce' or  \
+            user_choice_1 == 'nozyce' and user_choice_2 == 'papier':
+        print('Gracz1 wygral')
+        player1_score += 1
+    elif user_choice_1 == user_choice_2:
+        print('remis')
+    else:
+        print('Gracz dwa wygral')
+        player2_score += 1
