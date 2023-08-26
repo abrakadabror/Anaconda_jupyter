388621a4ee61f3f6dd38e8245795bd5fc529137b
Kamil <77681807+abrakadabror@users.noreply.github.com>
2023-08-26 13:49:41 +0200

    change the structure of if statment form two if to one and between first condttion  & second

--- test_01.py
+++ test_01.py
@@ -3,12 +3,18 @@ user_choice_1 = input(
 user_choice_2 = input(
     str('Graczu numer 2 wybierz jedna z opcji: kamien/papier/nozyce '))
 
-if user_choice_1 == 'papier':
-    if user_choice_2 == 'kamien':
-        print('Gracz1 wygral')
-elif user_choice_1 == 'kamien':
-    if user_choice_2 == 'nozyce':
-        print('Gracz1 wygral')
-elif user_choice_1 == 'nozyce':
-    if user_choice_2 == 'papier':
-        print('Gracz1 wygral')
+if user_choice_1 == 'papier' and user_choice_2 == 'kamien':
+    print('Gracz1 wygral')
+elif user_choice_1 == 'kamien' and user_choice_2 == 'nozyce':
+    print('Gracz1 wygral')
+elif user_choice_1 == 'nozyce' and user_choice_2 == 'papier':
+    print('Gracz1 wygral')
+else:
+    print('Gracz dwa wygral')
+
+
+# pojemnik = 4
+# pojemnik2 = 5
+
+# if pojemnik == 4 and pojemnik2 == 5:
+#     print('dziala')
