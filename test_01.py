d17ee38fb3950e4290fbff9e2df97810f6b05a38
Kamil <77681807+abrakadabror@users.noreply.github.com>
2023-08-26 13:41:04 +0200

    added elif instead of if

--- test_01.py
+++ test_01.py
@@ -1,6 +1,14 @@
-user_choice_1 = input(str('Gracz1 podaj swoj wybor '))
-user_choice_2 = input(str('Gracz1 podaj swoj wybor '))
+user_choice_1 = input(
+    str('Graczu numer 1 wybierz jedna z opcji: kamien/papier/nozyce '))
+user_choice_2 = input(
+    str('Graczu numer 2 wybierz jedna z opcji: kamien/papier/nozyce '))
 
 if user_choice_1 == 'papier':
     if user_choice_2 == 'kamien':
+        print('Gracz1 wygral')
+elif user_choice_1 == 'kamien':
+    if user_choice_2 == 'nozyce':
+        print('Gracz1 wygral')
+elif user_choice_1 == 'nozyce':
+    if user_choice_2 == 'papier':
         print('Gracz1 wygral')
