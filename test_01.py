6805224db1a7a67a1bad2e79df13a0da615de362
Kamil <77681807+abrakadabror@users.noreply.github.com>
2023-08-26 13:58:53 +0200

    the elif statement was eliminated and the "or" operator was used.

--- test_01.py
+++ test_01.py
@@ -3,12 +3,12 @@ user_choice_1 = input(
 user_choice_2 = input(
     str('Graczu numer 2 wybierz jedna z opcji: kamien/papier/nozyce '))
 
-if user_choice_1 == 'papier' and user_choice_2 == 'kamien':
+if user_choice_1 == 'papier' and user_choice_2 == 'kamien' or \
+        user_choice_1 == 'kamien' and user_choice_2 == 'nozyce' or  \
+        user_choice_1 == 'nozyce' and user_choice_2 == 'papier':
     print('Gracz1 wygral')
-elif user_choice_1 == 'kamien' and user_choice_2 == 'nozyce':
-    print('Gracz1 wygral')
-elif user_choice_1 == 'nozyce' and user_choice_2 == 'papier':
-    print('Gracz1 wygral')
+elif user_choice_1 == user_choice_2:
+    print('remis')
 else:
     print('Gracz dwa wygral')
 
