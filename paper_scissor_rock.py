b4d94f6da678aecbd825acc150bdf8c7cd7ddc61
Kamil <77681807+abrakadabror@users.noreply.github.com>
2023-08-28 13:39:46 +0200

    The game is almost ended. Furthermore Imported and added getpass method to anonymization the answers of the players

--- paper_scissor_rock.py
+++ paper_scissor_rock.py
@@ -1,3 +1,4 @@
+import getpass
 player1_score = 0
 player2_score = 0
 
@@ -5,15 +6,17 @@ options = ['kamien', 'papier', 'nozyce']
 while player1_score != 3 and player2_score != 3:
     users_choices = True
     while users_choices:
-        user_choice_1 = input(
+        user_choice_1 = getpass.getpass(
             'Graczu numer 1 wybierz jedna z opcji: kamien/papier/nozyce: ')
         if user_choice_1 in options:
             users_choices = False
+    users_choices = True
     while users_choices:
-        user_choice_2 = input(
+        user_choice_2 = getpass.getpass(
             'Graczu numer 2 wybierz jedna z opcji: kamien/papier/nozyce: ')
         if user_choice_2 in options:
             users_choices = False
+
     if user_choice_1 == 'papier' and user_choice_2 == 'kamien' or \
             user_choice_1 == 'kamien' and user_choice_2 == 'nozyce' or  \
             user_choice_1 == 'nozyce' and user_choice_2 == 'papier':
