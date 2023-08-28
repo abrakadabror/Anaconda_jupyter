5bceb46b04a1f8a6cf4de8c897afd97a2db40acb
Kamil <77681807+abrakadabror@users.noreply.github.com>
2023-08-28 15:58:54 +0200

    i've create another def which name is: check result to simplification the code

--- paper_scissor_rock.py
+++ paper_scissor_rock.py
@@ -4,28 +4,35 @@ options = ['kamien', 'papier', 'nozyce']
 
 
 def download_choice(gracz):
-    users_choices_is_correct = True
-    while users_choices_is_correct:
+    while True:
         user_choice = input(f' {gracz} podaj swoj wybor: ')
         if user_choice in options:
-            users_choices_is_correct = False
-    return user_choice
+            return user_choice
+
+
+def check_result(user_choice_1, user_choice_2):
+    if user_choice_1 == 'papier' and user_choice_2 == 'kamien' or \
+            user_choice_1 == 'kamien' and user_choice_2 == 'nozyce' or  \
+            user_choice_1 == 'nozyce' and user_choice_2 == 'papier':
+        print('Gracz1 wygral')
+        return 1
+    elif user_choice_1 == user_choice_2:
+        return 0
+    else:
+        print('Gracz dwa wygral')
+        return -1
 
 
 while player1_score != 3 and player2_score != 3:
     user_choice_1 = download_choice('Gracz1')
     user_choice_2 = download_choice('Gracz2')
+    score = check_result(user_choice_1, user_choice_2)
 
-    if user_choice_1 == 'papier' and user_choice_2 == 'kamien' or \
-            user_choice_1 == 'kamien' and user_choice_2 == 'nozyce' or  \
-            user_choice_1 == 'nozyce' and user_choice_2 == 'papier':
-        print('Gracz1 wygral')
+    if score == 1:
         player1_score += 1
-    elif user_choice_1 == user_choice_2:
-        print('remis')
-    else:
-        print('Gracz dwa wygral')
+    elif score == - 1:
         player2_score += 1
+
 if player1_score > player2_score:
     print('gre wygrywa gracz 1')
 elif player1_score < player2_score:
--- test.py
+++ test.py
@@ -1,10 +1,4 @@
-def make_a_sandwich(chlebek, samrowidlo, wedlina):
-    print('making a sandwich')
-    print(f' z chlebka {chlebek}')
-    print(f' do smarowania {samrowidlo}')
-    print(f' do poloznia na nakapke {wedlina}')
-    return 'jakis tekst testowy'
+nasz_slownik = {'adam': 32, 'marek': 18}
 
-
-make_a_sandwich('pszenny', 'maselko', 'szynka')
-# print(zmienna)
+print(nasz_slownik)
+print(nasz_slownik['adam'])
