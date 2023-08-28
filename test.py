884c16560c24c00d91000c1b2d4a9c901d97bd67
Kamil <77681807+abrakadabror@users.noreply.github.com>
2023-08-28 15:06:30 +0200

    creating a example of def function

--- test.py
+++ test.py
@@ -1,7 +1,10 @@
-def write():
-    print('Kamil')
-    print('Mariusz')
-    print('Konrad')
+def make_a_sandwich(chlebek, samrowidlo, wedlina):
+    print('making a sandwich')
+    print(f' z chlebka {chlebek}')
+    print(f' do smarowania {samrowidlo}')
+    print(f' do poloznia na nakapke {wedlina}')
+    return 'jakis tekst testowy'
 
 
-write()
+make_a_sandwich('pszenny', 'maselko', 'szynka')
+# print(zmienna)
